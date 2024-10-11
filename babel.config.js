de73c4ede1104f5b98e64ed33aa813dcc6abf4fe
plannist <plannist@cj.net>
2024-10-11 16:42:15 +0900

    first-commit

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset', "nativewind/babel"],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          "@": "./"
+        }
+      },
+    ],
+  ],
+};
